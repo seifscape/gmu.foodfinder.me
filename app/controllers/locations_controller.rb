ddd35f06de2a3d6559a506132e0e7a3c1edaced1
GSMcNamara <holler@incless.com>
2013-01-21 22:23:35 -0500

    Added a method to implement later and a note of future stuff TODO

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,14 @@
 class LocationsController < ApplicationController
   def index
 	#here we list all the locations that are open, by default
+
+	#TODO:
+	# => will need to have an alias column so a place like pilot house can have a nickname
+	# => such as: "I'm drunk and I don't want any damn karaoke (Ike's reference)"
+  end
+
+  def random
+  	#this is where you can go to get a random location and food suggestion from one of the places 
+  	# => that is currently open
   end
 end
