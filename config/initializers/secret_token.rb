e717e3173ef125d5e1cb38ab0e00c3d5ff324959
GSMcNamara <holler@incless.com>
2013-01-21 21:20:57 -0500

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatTheFuckShouldIEatGMU::Application.config.secret_token = '04dd671c145e1e5592b4336bfcc3cce7c4bf0b3867672da956be42f112c3176a64857e56f3e890a3a07a7ec1393bed2a3f36c7dbc2efac57a1cd49694853f949'
