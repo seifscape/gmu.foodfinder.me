ee47de761cf668330cb7f7e5831643d0b9b1207a
GSMcNamara <holler@incless.com>
2013-01-24 22:39:40 -0500

    attempting Heroku deployment issues fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,15 @@ WhatTheFuckShouldIEatGMU::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+ 
+
+
+
+  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
