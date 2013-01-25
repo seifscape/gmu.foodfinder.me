5f565b9108025260d5138779e50a849517f8b352
GSMcNamara <holler@incless.com>
2013-01-24 22:00:57 -0500

    bundle updating sqlite3

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Uses:
 - Use GPS information from mobile app users to show them on a Google Map distance to the food spots and walking directions (if available and useful)
 - Use Twitter Bootstrap to make this thing look nice and sexy?
 - Create a WTF Engine gem at the end of this project?
+- Figure out if should use Recurring for holidays and such? https://github.com/ggoodale/recurring/blob/master/lib/schedule.rb
+- buy: http://whatthefuckshouldieatatgmu.com/
+- Get Google Analytics working then deploy to Heroku
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Location.create!({:name => 'Pilot House', :aka => "I'm drunk and I don't want any damn karaoke", 
+Location.create!({:name => 'Pilot House', :aka => "I'm drunk, it's late, and I don't want any damn karaoke", 
 	:building => "Hampton Roads", :business_hours => {"sunday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
 												"monday" => ("00:00"..."04:00").to_a + ("17:00"..."24:00").to_a,
 												"tuesday" => ("00:00"..."04:00").to_a + ("17:00"..."24:00").to_a,
 												"wednesday" => ("00:00"..."04:00").to_a + ("17:00"..."24:00").to_a,
 												"thursday" => ("00:00"..."04:00").to_a + ("17:00"..."24:00").to_a,
 												"friday" => ("00:00"..."04:00").to_a + ("17:00"..."24:00").to_a,
+												"saturday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a}},
+												:without_protection => true)
+Location.create!({:name => "Ike's", :aka => "I'm drunk but I'm willing to put up with the karaoke and all the yellow", 
+	:building => "President's Park", :business_hours => {"sunday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
+												"monday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
+												"tuesday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
+												"wednesday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
+												"thursday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
+												"friday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a,
 												"saturday" => ("00:00"..."04:00").to_a + ("21:00"..."24:00").to_a}},
 												:without_protection => true)
